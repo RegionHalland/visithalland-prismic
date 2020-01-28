83d6591fd0e574e65cff28004ec63c830396bfba
David Öhlin <hello@davidohl.in>
2020-01-28 16:04:48 +0100

    fix linkresolver.....

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -1,16 +1,5 @@
 // https://github.com/birkir/gatsby-source-prismic-graphql/issues/42
 // https://prismic.io/docs/reactjs/beyond-the-api/link-resolving
-
-import { Link } from 'gatsby'
-import React from 'react'
-
-export function LinkResolver(type, element, content, children, index) {
-	if (element.data.link_type === 'Web') {
-		return <a href={element.data.url}>{content}</a>
-	}
-	return (
-		<Link key={element.data.id} to={element.data.uid}>
-			{content}
-		</Link>
-	)
+export function linkResolver(doc) {
+	if (doc.type === 'content') return `/content/${doc.uid}`
 }
