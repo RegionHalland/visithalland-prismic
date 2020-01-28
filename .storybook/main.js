407432f4e88dba84d776f2518f86f62494c6f08e
David Öhlin <hello@davidohl.in>
2020-01-28 11:20:34 +0100

    add tailwind and knobs to storybook :book:

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
 	stories: ['../src/**/*.stories.js'],
-	addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+	addons: [
+		'@storybook/addon-knobs',
+		'@storybook/addon-actions',
+		'@storybook/addon-links',
+	],
 }
