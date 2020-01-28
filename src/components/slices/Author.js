4a8a93fc865f0fd8f2287af217a93a61373d61ca
David Öhlin <hello@davidohl.in>
2020-01-28 14:24:47 +0100

    remove author image because it does not exisst

--- src/components/slices/Author.js
+++ src/components/slices/Author.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import Img from 'gatsby-image'
 import styled from 'styled-components'
 import tw from 'tailwind.macro'
 
-const Author = ({ name, role, email, phone, image }) => {
+const Author = ({ name, role, email, phone }) => {
 	return (
 		<div className="border p-3">
-			<Img
-				className="h-32 w-32 rounded-full"
-				fluid={image}
-				alt="hahahahahaha"
-			/>
 			<div>{name}</div>
 			<div>{role}</div>
 			<div>{phone}</div>
