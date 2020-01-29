b6e9b7e82fa331ce64d9658609a266662c02087f
David Öhlin <hello@davidohl.in>
2020-01-29 13:17:29 +0100

    delete test slice

--- src/components/SliceParser.js
+++ src/components/SliceParser.js
@@ -1,10 +1,8 @@
 import React from 'react'
 
-import TestContainer from './slices/TestContainer'
 import TextContainer from './slices/TextContainer'
 
 const TYPE_COMPONENTS = {
-	test: TestContainer,
 	text: TextContainer,
 }
 
@@ -12,7 +10,6 @@ const SliceParser = ({ slices }) => (
 	<main>
 		{slices.map((slice, index) => {
 			const Component = TYPE_COMPONENTS[slice.type] || null
-
 			return Component ? <Component slice={slice} key={index} /> : null
 		})}
 	</main>
