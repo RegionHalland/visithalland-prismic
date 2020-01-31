78cf522c88c3b998449c54117fbe022b04076d85
Albin Martinsson <albinmartinsson91@gmail.com>
2020-01-31 14:46:57 +0100

    Add caption to ImageFull

--- src/components/slices/ImageFull.js
+++ src/components/slices/ImageFull.js
@@ -2,10 +2,20 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import Img from 'gatsby-image'
 
-const ImageFull = ({ image }) => <Img fluid={image} className="w-full h-full" />
+const ImageFull = ({ image, caption }) => (
+	<figure className="w-full h-full">
+		{image && <Img fluid={image} className="w-full h-full mb-2" />}
+		{caption && (
+			<figcaption className="text-gray-600 font-sans italic text-sm block text-right">
+				{caption}
+			</figcaption>
+		)}
+	</figure>
+)
 
 ImageFull.propTypes = {
 	image: PropTypes.object.isRequired,
+	caption: PropTypes.string,
 }
 
 export default ImageFull
--- src/stories/ImageFull.stories.js
+++ src/stories/ImageFull.stories.js
@@ -6,8 +6,10 @@ import { ImageSquare } from './Image'
 
 import ImageFull from '../components/slices/ImageFull'
 
+const caption = 'This is a photograph of something.'
+
 storiesOf('ImageFull', module)
 	.addDecorator(withKnobs)
 	.add('default', () => {
-		return <ImageFull image={ImageSquare} />
+		return <ImageFull image={ImageSquare} caption={caption} />
 	})
