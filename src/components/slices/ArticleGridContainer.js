f0a5aa47499ac12f81892afe18b165ca411ab9e7
David Öhlin <hello@davidohl.in>
2020-02-24 14:47:17 +0100

    add large variant to article grid

--- src/components/slices/ArticleGridContainer.js
+++ src/components/slices/ArticleGridContainer.js
@@ -2,23 +2,34 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import get from 'lodash.get'
 
-import ArticleGrid from './ArticleGrid'
+import ArticleGridLarge from './ArticleGridLarge'
+import ArticleGridSmall from './ArticleGridSmall'
 
 const ArticleGridContainer = ({ slice }) => {
-	const { fields } = slice
+	const { fields, primary } = slice
 
-	if (!fields || !Array.isArray(fields)) {
+	if (!fields || !Array.isArray(fields) || !primary) {
 		return null
 	}
 
-	const items = fields.map(({ article_grid_relationship: item }) => ({
-		title: get(item, 'title', ''),
-		meta: get(item, '_meta', {}),
-		tags: get(item, '_meta.tags', []),
-		image: get(item, 'seo_featured_imageSharp.fluid', null),
+	const variant = get(primary, 'article_grid_variant', 'small').toLowerCase()
+	const items = fields.map(item => ({
+		title: get(item, 'article_grid_relationship.title', ''),
+		meta: get(item, 'article_grid_relationship._meta', {}),
+		tags: get(item, 'article_grid_relationship._meta.tags', []),
+		buttonLabel: get(item, 'article_grid_button_label', ''),
+		image: get(
+			item,
+			'article_grid_relationship.seo_featured_imageSharp.fluid',
+			null,
+		),
 	}))
 
-	return <ArticleGrid items={items} />
+	if (variant === 'large') {
+		return <ArticleGridLarge items={items} />
+	}
+
+	return <ArticleGridSmall items={items} />
 }
 
 ArticleGridContainer.propTypes = {
--- src/fragments/ArticleGridFragment.js
+++ src/fragments/ArticleGridFragment.js
@@ -3,7 +3,11 @@ import { graphql } from 'gatsby'
 export const ArticleGridFragment = graphql`
 	fragment ArticleGridFragment on PRISMIC_ContentBodyArticle_grid {
 		type
+		primary {
+			article_grid_variant
+		}
 		fields {
+			article_grid_button_label
 			article_grid_relationship {
 				... on PRISMIC_Content {
 					title
