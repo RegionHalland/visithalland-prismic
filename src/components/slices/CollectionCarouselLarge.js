825847cffd3e50ae1509e84834a7dcfb2a62430f
David Öhlin <hello@davidohl.in>
2020-02-28 14:44:03 +0100

    wip: collection carousel large

--- src/components/slices/CollectionCarouselLarge.js
+++ src/components/slices/CollectionCarouselLarge.js
@@ -1,22 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Carousel from 'nuka-carousel'
 import Link from 'gatsby-link'
-import { linkResolver } from '../../utils/linkResolver'
-import Image from '../Image'
+import styled from 'styled-components'
 
-const CollectionCarouselLarge = ({ items }) => (
-	<div>
-		{items.map(({ title, tags, meta, image }, index) => (
-			<div key={`${title}-${index}`}>
-				<Image fluid={image} />
-				{tags.map(tag => (
-					<span key={tag}>{tag}</span>
-				))}
-				<Link to={linkResolver(meta)}>{title}</Link>
-			</div>
-		))}
-	</div>
-)
+import Button from '../Button'
+import Image from '../Image'
+import CarouselArrow from '../CarouselArrow'
+
+import { linkResolver } from '../../utils/linkResolver'
+
+const CollectionCarouselLarge = ({ items }) => {
+	return (
+		<Carousel
+			className="border-4"
+			renderBottomCenterControls={() => null}
+			renderCenterLeftControls={({ previousSlide }) => (
+				<CarouselArrow onClick={previousSlide} direction="left" />
+			)}
+			renderCenterRightControls={({ nextSlide }) => (
+				<CarouselArrow onClick={nextSlide} direction="right" />
+			)}
+		>
+			{items.map(({ title, meta, image }, index) => (
+				<div key={`${index}-${meta.uid}`}>
+					<Image fluid={image} />
+				</div>
+			))}
+		</Carousel>
+	)
+}
+
+const Center = styled.div`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+`
 
 CollectionCarouselLarge.propTypes = {
 	items: PropTypes.array.isRequired,
