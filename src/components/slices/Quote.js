9e5224d4dccc3d9373e3282fb78e24a53c2dd90c
Albin Martinsson <albinmartinsson91@gmail.com>
2020-03-10 13:53:25 +0100

    Update Quote.js

--- src/components/slices/Quote.js
+++ src/components/slices/Quote.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { linkResolver } from '../../utils/linkResolver'
-import { Parallax } from 'react-scroll-parallax'
+
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../../tailwind.config.js'
+
 import Image from '../Image'
 import ButtonLink from '../ButtonLink'
 import Container from '../Container'
 import styled from 'styled-components'
-import BackgroundPattern from '../../images/bg-pattern.svg'
+
+const config = resolveConfig(tailwindConfig)
 
 const Quote = ({ quote, byline, image, meta, buttonLabel, url }) => (
 	<div className="bg-sand-300 py-12 mt-32">
 		<Container className="px-4 lg:px-6 flex flex-wrap -mx-3" tight>
-			<div className="h-40vh lg:h-60vh w-full md:w-6/12 px-3 rounded overflow-hidden relative -mt-32 mb-6 md:mb-0">
+			<ImageContainer className="w-full md:w-6/12 px-3 rounded overflow-hidden relative -mt-32 mb-6 md:mb-0">
 				<Image
 					style={{ position: 'absolute' }}
 					className="h-full w-full bottom-0 top-0 left-0 z-0"
@@ -19,7 +23,7 @@ const Quote = ({ quote, byline, image, meta, buttonLabel, url }) => (
 					objectPosition="50% 50%"
 					fluid={image}
 				/>
-			</div>
+			</ImageContainer>
 
 			<div className="w-full md:w-6/12 md:pl-6">
 				<blockquote className="text-2xl md:text-3xl mx-auto mb-6 w-full leading-tight text-black font-semibold">
@@ -37,6 +41,14 @@ const Quote = ({ quote, byline, image, meta, buttonLabel, url }) => (
 		</Container>
 	</div>
 )
+
+const ImageContainer = styled.div`
+	height: 400px;
+	@media (min-width: ${config.theme.screens.lg}) {
+		height: 500px;
+	}
+`
+
 Quote.propTypes = {
 	quote: PropTypes.string.isRequired,
 	byline: PropTypes.string,
