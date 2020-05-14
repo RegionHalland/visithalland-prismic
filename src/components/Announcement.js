5c7db9949cbbf05e25cfd305e1b2d155b1c051ee
Albin Martinsson <albinmartinsson91@gmail.com>
2020-05-14 08:54:45 +0200

    Add new styles for announcement

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -5,23 +5,33 @@ import BellIcon from './icons/BellIcon'
 import CloseIcon from './icons/CloseIcon'
 import IconButton from './IconButton'
 
+import styled from 'styled-components'
+import tw from 'tailwind.macro'
+
 const Announcement = ({ content, id, onClose }) => {
 	return (
-		<div className="flex items-start md:items-center text-blue-700 border-b border-gray-300 bg-gray-200 justify-between px-4 py-2 text-base relative z-50">
-			<BellIcon className="text-gray-600 mr-4 mt-2 h-4 w-4 md:mt-0" />
+		<div className="flex items-start md:items-center text-blue-700 border-b border-gray-300 bg-green-200 justify-between px-4 py-2 text-base relative z-50">
+			<BellIcon className="text-green-600 mr-4 mt-2 h-4 w-4 md:mt-0" />
 
-			<div className="flex-1 mb-2 mr-6 md:mb-0">
+			<StyledText className="flex-1 mb-2 mr-6 md:mb-0">
 				<TextRenderer text={content} />
-			</div>
+			</StyledText>
 			<IconButton
 				size="small"
-				className="self-center bg-green"
+				className="self-center"
+				colorscheme="green"
 				icon={<CloseIcon height={10} width={10} />}
 				onClick={() => onClose(id)}
 			/>
 		</div>
 	)
 }
+
+const StyledText = styled.div`
+	& p > a {
+		${tw`underline`}
+	}
+`
 
 Announcement.propTypes = {
 	content: PropTypes.array.isRequired,
